de3a893a0a4b586919a135d8338ba5dda0aa345f
Steven Cannon <stevencannon01@gmail.com>
2018-09-19 11:15:16 -0600

    getting student info

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo = {}
+    }
   }
-
+  componentDidMount() {
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(res => {
+      console.log(res)
+      this.setState({
+        studentInfo = res.data
+      })
+    })
+  }
   render() {
     return (
       <div className="box">
